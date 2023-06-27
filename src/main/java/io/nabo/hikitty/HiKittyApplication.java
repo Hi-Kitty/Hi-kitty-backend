6289fd2d0fd33a00bd3f483f44c0647a31b4eef3
Chan <33cks1423@naver.com>
2023-06-27 19:27:20 +0900

    [#1] feat: 프로젝트 초기 설정

---
+++ src/main/java/io/nabo/hikitty/HiKittyApplication.java
@@ -0,0 +1,13 @@
+package io.nabo.hikitty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiKittyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiKittyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/nabo/hikitty/HiKittyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nabo.hikitty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiKittyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
