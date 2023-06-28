f74323a58fcef2803ebc61595a109c35d95edc7a
Chan <33cks1423@naver.com>
2023-06-28 15:20:29 +0900

    [#12] feat: OpenAPI 초기 설정

--- src/main/java/io/nuabo/hikitty/HiKittyApplication.java
+++ src/main/java/io/nuabo/hikitty/HiKittyApplication.java
@@ -1,9 +1,9 @@
-package io.nabo.hikitty;
+package io.nuabo.hikitty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "io.nuabo")
 public class HiKittyApplication {
 
     public static void main(String[] args) {
--- src/main/java/io/nuabo/hikitty/common/presentation/HealthCheckController.java
+++ src/main/java/io/nuabo/hikitty/common/presentation/HealthCheckController.java
@@ -1,16 +1,20 @@
-package io.nabo.hikitty.common.application;
+package io.nuabo.hikitty.common.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "헬스 체크")
 @RestController
 public class HealthCheckController {
 
+    @Operation(summary = "헬스 체킹 API")
     @GetMapping("/health_check.html")
     public ResponseEntity<Void> healthCheck() {
         return ResponseEntity
-            .ok()
-            .build();
+                .ok()
+                .build();
     }
 }
