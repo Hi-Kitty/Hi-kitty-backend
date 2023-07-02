5ed7920ed1608edad35631896de2b00c70d4a792
Chan <33cks1423@naver.com>
2023-07-02 21:00:02 +0900

    [#28] refactor: Validation 설정

--- src/main/java/io/nuabo/hikitty/board/presentation/request/BoardCreateRequest.java
+++ src/main/java/io/nuabo/hikitty/board/presentation/request/BoardCreateRequest.java
@@ -2,7 +2,10 @@ package io.nuabo.hikitty.board.presentation.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -13,20 +16,24 @@ public class BoardCreateRequest {
 
     @NotNull(message = "title cannot be null")
     @Schema(description = "title",  example = "제목을 입력해주세요.")
+    @Size(min=1, max=255, message = "제목은 1자 이상 255자 이하로 입력해주세요.")
     private final String title;
 
     @Schema(description = "subTitle",  example = "부제목을 입력해주세요 - 조회하기에서 보여서 만듬!")
+    @Size(min=1, max=255, message = "부제목은 1자 이상 255자 이하로 입력해주세요.")
     private final String subTitle;
 
     @NotNull(message = "content cannot be null")
-    @Schema(description = "content",  example = "내용을 입력해주세요.")
+    @Schema(description = "content",  example = "내용을 입력해주세요. - 길게 적어주세용(4GB)")
     private final String content;
 
-    @NotNull(message = "targetAmount cannot be null")
-    @Schema(description = "targetAmount",  example = "목표금액을 입력해주세요.")
+    @NotNull(message = "targetAmount cannot be null,  more than 1000")
+    @Min(value = 1000, message = "목표금액은 1000원 이상이어야 합니다.")
+    @Schema(description = "targetAmount",  example = "1000")
     private final Long targetAmount;
 
-    @NotNull(message = "endAt cannot be null")
+    @NotNull(message = "endAt cannot be null, future is possible")
+    @Future(message = "종료일은 현재보다 미래여야 합니다.")
     @Schema(description = "endAt",  example = "종료일을 입력해주세요.")
     private final LocalDateTime endAt;
 
