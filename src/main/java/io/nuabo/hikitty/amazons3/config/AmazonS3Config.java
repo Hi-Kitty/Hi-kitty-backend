96930173c79d56c07f1f6e0091620e2749ef5171
Chan <33cks1423@naver.com>
2023-07-11 15:17:58 +0900

    [#70] !hotfix: 클라우드 워치 제거

--- src/main/java/io/nuabo/hikitty/amazons3/config/AmazonS3Config.java
+++ src/main/java/io/nuabo/hikitty/amazons3/config/AmazonS3Config.java
@@ -7,9 +7,6 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
 
 @Configuration
 public class AmazonS3Config {
@@ -27,12 +24,6 @@ public class AmazonS3Config {
     public BasicAWSCredentials basicAWSCredentials() {
         return new BasicAWSCredentials(accessKey, secretKey);
     }
-
-    @Bean
-    public AwsBasicCredentials awsBasicCredentials() {
-        return AwsBasicCredentials.create(accessKey, secretKey);
-    }
-
     @Bean
     public AmazonS3Client amazonS3Client(BasicAWSCredentials credentials) {
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
@@ -40,15 +31,5 @@ public class AmazonS3Config {
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .build();
     }
-
-    @Bean
-    public CloudWatchAsyncClient cloudWatchAsyncClient() {
-        return CloudWatchAsyncClient.builder()
-                .credentialsProvider(this::awsBasicCredentials)
-                .region(Region.AP_NORTHEAST_2)
-                .build();
-    }
-
-
 }
 
